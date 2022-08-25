265c0287c671021f762720ad6dccb3ba0eafe1c2
ms0130k <minseok0130@gmail.com>
2022-08-26 04:55:03 +0900

    update

--- src/main/java/com/shock/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/shock/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import java.util.Map;
 
 @Getter
-@Builder
 public class OAuthAttributes {
     private Map<String, Object> attributes;
     private String nameAttributeKey;
@@ -16,19 +15,19 @@ public class OAuthAttributes {
     private String email;
     private String picture;
 
-    //    @Builder
-//    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
-//        this.attributes = attributes;
-//        this.nameAttributeKey = nameAttributeKey;
-//        this.name = name;
-//        this.email=email;
-//        this.picture=picture;
-//    }
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
